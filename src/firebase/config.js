bc096aeaddcc4fd868dbd29cd31192a8892f7349
saihtutkhaungmyint <saihtutkhaungmyint@gmail.com>
2022-07-11 21:30:03 +0630

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB9BCpyVC74a8-wmQcrRdX0YQKbWBJP7rY',
+  authDomain: 'my-money-a50ea.firebaseapp.com',
+  projectId: 'my-money-a50ea',
+  storageBucket: 'my-money-a50ea.appspot.com',
+  messagingSenderId: '943651327172',
+  appId: '1:943651327172:web:1856d9bc05c307daf60d91',
+};
+
+//   init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
