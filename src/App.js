b7118265cef3ca1122d2afd834d0422d8b3d743c
saihtutkhaungmyint <saihtutkhaungmyint@gmail.com>
2022-07-11 21:02:24 +0630

    Creating Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// styles
+// import './App.css';
+
+// page components
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/signup'>
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
