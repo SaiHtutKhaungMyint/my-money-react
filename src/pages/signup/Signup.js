5f009b5db731cab6857507566835823c8470ed7a
saihtutkhaungmyint <saihtutkhaungmyint@gmail.com>
2022-07-11 21:25:37 +0630

    Making the Signup Form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,50 @@
+import { useState } from 'react';
+
+// styles
 import styles from './Signup.module.css';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(displayName, email, password);
+  };
+
+  return (
+    <form className={styles['signup-form']} onSubmit={handleSubmit}>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <label>
+        <span>display name:</span>
+        <input
+          type='text'
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+
+      <button className='btn'>Sign Up</button>
+    </form>
+  );
 };
 
 export default Signup;
